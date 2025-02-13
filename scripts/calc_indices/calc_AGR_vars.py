fixed bug in AGR spread estimate calculation

scripts/calc_indices/calc_AGR_vars.py
@@ -375,7 +375,7 @@ def calc_spread_estimates(gdata, data, areas, afacs=False):
         if data_var not in data.data_vars:
             continue
         c_upp = xr.full_like(gdata[vvar], 1)
-        c_upp = c_upp.where(gdata[vvar] >= data[data_var], 0)
+        c_upp = c_upp.where(data[data_var] >= gdata[vvar], 0)
 
         # calc upper spread (Eq. 38_2, 38_5)
         wgt_fac_u = 1 / (c_upp * areas).sum(dim=('lat', 'lon'))