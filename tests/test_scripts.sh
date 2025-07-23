2a054e0f30df1aad003ed40f423413afc697e311
Jürgen Fuchsberger <juergen.fuchsberger@uni-graz.at>
2025-07-23 14:05:22 +0000

    run calc_tea with minimal config in test_script.sh

--- tests/test_scripts.sh
+++ tests/test_scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-calc_tea --help && prep_ERA5 --help && prep_ERA5Land --help && create_region_masks --help &&
+calc_tea -cf ../src/teametrics/config/TEA_CFG_minimal.yaml && prep_ERA5 --help && prep_ERA5Land --help && create_region_masks --help &&
 regrid_SPARTACUS_to_WEGNext --help && tea_example --no-gui
