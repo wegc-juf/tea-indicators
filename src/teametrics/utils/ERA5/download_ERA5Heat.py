81d0acdc90871ad876114f54e3f96c3063f087aa
Stephanie Haas <stephanie.haas@uni-graz.at>
2025-08-04 09:01:09 +0000

    fixed download script for ERA5Heat data

--- src/teametrics/utils/ERA5/download_ERA5Heat.py
+++ src/teametrics/utils/ERA5/download_ERA5Heat.py
@@ -1,80 +1,49 @@
 """
-API request to download ERA5 Heat data -- NOT WORKING AT THE MOMENT!!!
+API request to download ERA5 Heat data
 """
-
 import cdsapi
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
-
-def retrieve(client, request, year):
-    print(f"requesting year: {year} /n")
-    request.update({"year": year})
-    client.retrieve(
-        "derived-utci-historical", request, f"ERA5Heat_{year}.nc"
-    ).download()
-    return f"retrieved year: {year}"
-
-
-def main(_request):
-    """concurrent request using 10 threads"""
-    client = cdsapi.Client()
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        futures = [
-            executor.submit(retrieve, client, _request.copy(), year) for year in YEARS
-        ]
-        for f in as_completed(futures):
-            try:
-                print(f.result())
-            except:
-                print("could not retrieve")
-
 
 if __name__ == "__main__":
-    YEARS = ["%d" % (y) for y in range(1961, 2024)]
+    YEARS = ["%d" % y for y in range(1961, 2025)]
 
-    dataset = "derived-utci-historical"
-    request = {
-        "variable": [
-            "universal_thermal_climate_index",
-            "mean_radiant_temperature"
-        ],
-        "version": "1_1",
-        "product_type": "consolidated_dataset",
-        "year": "",
-        "month": [
-            "01", "02", "03",
-            "04", "05", "06",
-            "07", "08", "09",
-            "10", "11", "12"
-        ],
-        "day": [
-            "01", "02", "03",
-            "04", "05", "06",
-            "07", "08", "09",
-            "10", "11", "12",
-            "13", "14", "15",
-            "16", "17", "18",
-            "19", "20", "21",
-            "22", "23", "24",
-            "25", "26", "27",
-            "28", "29", "30",
-            "31"
-        ],
-        "area": [72, -30, 30, 45]
-    }
+    for yr in YEARS:
+        request = {
+            "variable": ["universal_thermal_climate_index"],
+            "version": "1_1",
+            "product_type": "consolidated_dataset",
+            "year": [yr],
+            "month": [
+                "01", "02", "03",
+                "04", "05", "06",
+                "07", "08", "09",
+                "10", "11", "12"
+            ],
+            "day": [
+                "01", "02", "03",
+                "04", "05", "06",
+                "07", "08", "09",
+                "10", "11", "12",
+                "13", "14", "15",
+                "16", "17", "18",
+                "19", "20", "21",
+                "22", "23", "24",
+                "25", "26", "27",
+                "28", "29", "30",
+                "31"
+            ],
+            "area": [72, -30, 30, 45]
+        }
 
-    main(request)
+        client = cdsapi.Client()
+        client.retrieve("derived-utci-historical", request,
+                        f'ERA5Heat_{yr}.nc')
 
-
-# original request from CDS (not working either) ---------------------------------------------------
+# original request from CDS ---------------------------------------------------
 # import cdsapi
 #
 # dataset = "derived-utci-historical"
 # request = {
-#     "variable": [
-#         "universal_thermal_climate_index",
-#         "mean_radiant_temperature"
-#     ],
+#     "variable": ["universal_thermal_climate_index"],
 #     "version": "1_1",
 #     "product_type": "consolidated_dataset",
 #     "year": ["1961"],
